7539ff0f8201be760a2d9773cbe80ec4f44d85bc
Paul Blouët <paul@paulblouet.fr>
2013-06-25 10:09:04 +0200

    test version for 0.1

--- main.js
+++ main.js
@@ -1,6 +1,54 @@
-// Copyright 2013 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var mappings = {
+"BackQuote":"$",
+"Digit1":"\"",
+"Digit2":"«",
+"Digit3":"»",
+"Digit4":"(",
+"Digit5":")",
+"Digit6":"@",
+"Digit7":"+",
+"Digit8":"-",
+"Digit9":"/",
+"Digit0":"*",
+"Minus":"=",
+"Equal":"%",
+"KeyQ":"b",
+"KeyW":"é",
+"KeyE":"p",
+"KeyR":"o",
+"KeyT":"è",
+"KeyY":"^",
+"KeyU":"v",
+"KeyI":"d",
+"KeyO":"l",
+"KeyP":"j",
+"BracketLeft":"z",
+"BlacketRight":"w",
+"KeyA":"a",
+"KeyS":"u",
+"KeyD":"i",
+"KeyF":"e",
+"KeyG":",",
+"KeyH":"c",
+"KeyJ":"t",
+"KeyK":"s",
+"KeyL":"r",
+"Semicolon":"n",
+"Quote":"m",
+"Backslash":"ç",
+"IntlBackslash":"ê",
+"KeyZ":"à",
+"KeyX":"y",
+"KeyC":"x",
+"KeyV":".",
+"KeyB":"k",
+"KeyN":"'",
+"KeyM":"q",
+"Comma":"g",
+"Period":"h",
+"Slash":"f",
+"Space":" "
+}
 
 var ime_api = chrome.input.ime;
 
@@ -15,9 +63,9 @@ ime_api.onBlur.addListener(function(contextID) {
 
 ime_api.onKeyEvent.addListener(
 function(engineID, keyData) {
-  if (keyData.type == "keydown") {
+  if (keyData.type == "keydown" && mappings.hasOwnProperty(keyData.code)) {
     chrome.input.ime.commitText({"contextID": context_id,
-                                 "text": keyData.code});
+                                 "text": mappings[keyData.code]});
     return true;
   }
 
